da23c610d34675349a913a4f385c499275bb939b
Bruno Santana <BrunoSSantana@users.noreply.github.com>
2020-08-13 20:56:22 -0300

    :construction:

--- server.js
+++ server.js
@@ -1,39 +1,21 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const routes = require('./routes')
+const methodOverride = require('method-override')
 
 const server = express()
-const datas = require("./data")
 
+server.use(express.urlencoded( {extended: true} ))
 server.use(express.static('public'))
+server.use(methodOverride('_method'))
+server.use(routes)
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
     express: server,
-    noCache: false
-})
-
-server.get("/", function(req, res) {
-    return res.render('home', {datas})
-})
-server.get("/receitas", function(req, res) {
-    return res.render('receitas', {datas})
-})
-server.get("/receitas/:id", function(req, res) {
-    const id = req.params.id
-
-    const receita = datas.find( function(receita) {
-        if(receita.id == id){
-            return true
-        } else if(!receita){
-            return res. send(`Receita nã encontrada`)
-        }
-    })
-    return res.render("prato", {receita})
-})
-
-server.get("/sobre", function(req, res) {
-    return res.render('sobre')
+    autoescape: false,
+    noCache: true
 })
 
 server.listen(5000, function() {
