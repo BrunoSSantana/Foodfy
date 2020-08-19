99dfc10d6ec637ca3c5c28a8bf4f46dea2abede8
BrunoSSantana <brunoosouza15@gmail.com>
2020-08-19 11:44:04 -0300

    :construction: post init

--- public/utils.js
+++ public/utils.js
@@ -1,21 +1,19 @@
-// const ul = document.querySelector()
-// const button = document.querySelectorAll()
-// const input = document.querySelector()
-
-arrayElement = [
-    "Bruno",
-    "Vida",
-    "Ana"
-]
-
-function renderElements() {
-    for (item of arrayElement) {
-        const li = document.createAttribute('li')
-        const txt = document.createTextNode(item)
-
-        li.appendChild(txt)
-        
-    }
-}
-
-renderElements()
+function addIngredient() {
+    const ingredients = document.querySelector("#ingredients");
+    const fieldContainer = document.querySelectorAll(".ingredient");
+  
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+  
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == "") return false;
+  
+    // Deixa o valor do input vazio
+    newField.children[0].value = "";
+    ingredients.appendChild(newField);
+  }
+  
+  document
+    .querySelector(".add-ingredient")
+    .addEventListener("click", addIngredient);
+//================>>>>>>===============>>>>>>===>>
