47710ac36c13942f19e6fe7412e52c197a003877
BrunoSSantana <brunoosouza15@gmail.com>
2020-11-17 07:12:34 -0300

    working challenge foodfy

--- src/app/controllers/chefs.js
+++ src/app/controllers/chefs.js
@@ -1,144 +1,133 @@
 const data = require('../../data.json')
 const fs = require('fs')
-
-/* === Admin === */ 
-
-exports.chefs = function(req, res) { // ok
-    return res.render('admin/chefs/listing', {chefs: data.chef})
-}
-//================================================
-exports.show = function(req, res) { //ok
-    const id = req.params.id
-
-    const chef = data.chef.find( function(receita) {
-        if(receita.id == id){
-            return true
-        } else if(!receita){
-            return res.send(`Receita não encontrada`)
-        }
-    })
-    return res.render("admin/chefs/details", {chef})
-}
-//================================================
-exports.create = function(req, res) {
-    return res.render('admin/chefs/create')
-}
-//================================================
-exports.post = function(req, res) { //ok
-    const keys = Object.keys(req.body)
+module.exports = {
+    /* === Admin === */ 
+    chefs(req, res) { // ok
+        return res.render('admin/chefs/listing', {chefs: data.chef})
+    },
+    show(req, res) { //ok
+        const id = req.params.id
     
-    for (key of keys) {
-        if (req.body[key] == "") {
-            return res.send(`Preencha todos os campos`)
+        const chef = data.chef.find( function(receita) {
+            if(receita.id == id){
+                return true
+            } else if(!receita){
+                return res.send(`Receita não encontrada`)
+            }
+        })
+        return res.render("admin/chefs/details", {chef})
+    },
+    create(req, res) {
+        return res.render('admin/chefs/create')
+    },
+    post(req, res) { //ok
+        const keys = Object.keys(req.body)
+        
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send(`Preencha todos os campos`)
+            }
         }
-    }
+        
+        let {title, image, ingredients, preparation, information, author} = req.body
+        const id = Number(data.length + 1)
     
-    let {title, image, ingredients, preparation, information, author} = req.body
-    const id = Number(data.length + 1)
-
-    datas.chef.push({
-        image,
-        title,
-        id,
-        author,
-        ingredients,
-        preparation,
-        information
-    })
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if (err) return res.send('Write file err')
-
-        return res.redirect("/admin/chefs", {chef: data.chefs})
-    })
-
-}
-//================================================
-exports.edit = function(req, res) {
-    const {id} = req.params
-
-    const receita = data.chef.find((receita)=>{
-        if(receita.id == id) {
-            return true
-        } else if(!receita) {
-            return res.send(`Receita não encontrada`)
+        datas.chef.push({
+            image,
+            title,
+            id,
+            author,
+            ingredients,
+            preparation,
+            information
+        })
+    
+        fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+            if (err) return res.send('Write file err')
+    
+            return res.redirect("/admin/chefs", {chef: data.chefs})
+        })
+    
+    },
+    edit(req, res) {
+        const {id} = req.params
+    
+        const receita = data.chef.find((receita)=>{
+            if(receita.id == id) {
+                return true
+            } else if(!receita) {
+                return res.send(`Receita não encontrada`)
+            }
+        })
+    
+        return res.render("admin/chefs/edit", {chef: receita})
+    },
+    put(req, res) {
+        const {id} = req.body
+        let index = 0
+    
+        function founfor(element, foundindex) {
+            if (element.id == id){
+                index = foundindex
+                return element
+            }
+            if (!element) {
+                return res.send('Receita não encontrada')
+            }
         }
-    })
-
-    return res.render("admin/chefs/edit", {chef: receita})
-}
-//================================================
-exports.put = function(req, res) {
-    const {id} = req.body
-    let index = 0
-
-    function founfor(element, foundindex) {
-        if (element.id == id){
-            index = foundindex
-            return element
+    
+        const foundchef = data.chef.find(founfor)
+    
+        const chef = {
+            ...foundchef,
+            ...req.body,
+            id: Number(req.body.id)
         }
-        if (!element) {
-            return res.send('Receita não encontrada')
-        }
+    
+        data.chef[index] = chef
+    
+    
+        fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+            if (err) return res.send('Write file err')
+    
+        return res.redirect(`/admin/chefs/${id}`)//err express res.redirect
+        })
+    },
+    delete(req, res) {
+        const {id} = req.body
+    
+        const filterchefs = data.chef.filter(function(chef){
+            return chef.id != id
+        })
+    
+        data.chef = filterchefs
+    
+        fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+            if(err) return res.send("Erro na execução do processo")
+    
+            return res.redirect("/admin/chefs/")
+        })
+    },
+    /*----Users----*/ 
+    home(req, res) { //ok
+        return res.render('home', {chefs: data.chef})
+    },
+    about(req, res) { //ok
+        return res.render('sobre')
+    },
+    showUsers(req, res) { //ok
+        const id = req.params.id
+    
+        const chef = data.chef.find( function(receita) {
+            if(receita.id == id){
+                return true
+            } else if(!receita){
+                return res.send(`Receita não encontrada`)
+            }
+        })
+        return res.render("receita", {chef})
+    },
+    chefsUsers(req, res) { // ok
+        return res.render('receitas', {chef: data.chef})
     }
-
-    const foundchef = data.chef.find(founfor)
-
-    const chef = {
-        ...foundchef,
-        ...req.body,
-        id: Number(req.body.id)
-    }
-
-    data.chef[index] = chef
-
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if (err) return res.send('Write file err')
-
-    return res.redirect(`/admin/chefs/${id}`)//err express res.redirect
-    })
-}
-//================================================
-exports.delete = function(req, res) {
-    const {id} = req.body
-
-    const filterchefs = data.chef.filter(function(chef){
-        return chef.id != id
-    })
-
-    data.chef = filterchefs
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
-        if(err) return res.send("Erro na execução do processo")
-
-        return res.redirect("/admin/chefs/")
-    })
-}
-
-/* === Users === */ 
-
-exports.home = function(req, res) { //ok
-    return res.render('home', {chefs: data.chef})
-}
-//================================================
-exports.about = function(req, res) { //ok
-    return res.render('sobre')
-}
-//================================================
-exports.showUsers = function(req, res) { //ok
-    const id = req.params.id
-
-    const chef = data.chef.find( function(receita) {
-        if(receita.id == id){
-            return true
-        } else if(!receita){
-            return res.send(`Receita não encontrada`)
-        }
-    })
-    return res.render("receita", {chef})
-}
-//================================================
-exports.chefsUsers = function(req, res) { // ok
-    return res.render('receitas', {chef: data.chef})
 }
